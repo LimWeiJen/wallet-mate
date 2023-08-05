ea01ba96a3e4cede3ed60ceaf970fe94e2240b54
LimWeiJen <limweijen96@gmail.com>
2023-08-05 10:32:04 +0800

    style: Add styles for landing page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,15 +1,19 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className="bg-bg">
-     <h1 className='text-7xl font-bold'>
+    <main className="bg-bg grid place-content-center h-screen place-items-center">
+     <h1 className='text-7xl font-bold text-center my-2'>
       Wallet Mate
      </h1>
-     <h3 className='text-3xl'>
+     <h3 className='text-3xl text-center my-2'>
       simple. efficient. convenient
      </h3>
-     <button className='text-2xl gradient-1 px-5 py-1 rounded-full'>Use Now</button>
+     <button className='text-xl transition-all gradient-1 py-2 font-bold rounded-lg w-2/5 my-4 hover:rotate-1 hover:scale-105'>Use Now</button>
+     <p className="text-xs font-thin absolute bottom-0 m-5">created by @limweijen ^_^</p>
+     <div>
+      <div className="circle gradient-1 top-0" style={{animation: 'circleAnimation1 10s infinite ease-in-out'}}></div>
+      <div className="circle gradient-1 top-32 left-36" style={{animation: 'circleAnimation2 10s infinite ease-in-out'}}></div>
+      <div className="circle gradient-1 top-48 left-96" style={{animation: 'circleAnimation3 10s infinite ease-in-out'}}></div>
+     </div>
     </main>
   )
 }
