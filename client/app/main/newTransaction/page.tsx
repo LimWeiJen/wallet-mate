e02c4a3ec99a05d90b6ba0ea6b3f07af4f832bb7
LimWeiJen <limweijen96@gmail.com>
2023-08-22 16:12:42 +0800

    feat: Add category option in adding a new transaction

--- client/app/main/newTransaction/page.tsx
+++ client/app/main/newTransaction/page.tsx
@@ -8,6 +8,7 @@ const NewTransaction = () => {
   const amount = useRef<any>(null);
   const description = useRef<any>(null);
   const account = useRef<any>(null);
+  const category = useRef<any>(null);
   const [error, setError] = useState('');
   const [availableAccounts, setAvailableAccounts] = useState<Array<string>>([]);
 
@@ -36,7 +37,8 @@ const NewTransaction = () => {
           description: description.current.value!,
           accountId: account.current.value!,
           amount: amount.current.value!,
-          type: isIncome ? 'income' : 'expenses'
+          type: isIncome ? 'income' : 'expenses',
+          category: category.current.value!
         }
       })
     })
@@ -70,9 +72,25 @@ const NewTransaction = () => {
       <input ref={amount} className='p-3 w-11/12 font-bold text-2xl bg-primaryBlack bg-gradient-to-r from-secondaryGreen to-[#223D3D1e] rounded-t-lg' type="number" placeholder='Amount' />
       <div className='h-1 w-11/12 gradient-1 mb-3'></div>
       <input ref={description} className='p-3 w-11/12 bg-primaryBlack my-3 bg-gradient-to-r from-secondaryGreen to-[#223d3d1e] rounded-lg' type="text" placeholder="What's the income for?" />
-      <select ref={account} className='p-3 w-11/12 bg-primaryBlack my-3 bg-gradient-to-r from-secondaryGreen to-[#223D3D1e] rounded-lg focus:outline-none focus:border-none'>
-        {availableAccounts.map(account => <option value={account}>{account}</option>)}
-      </select>
+      <div className='flex justify-between w-11/12'>
+        <select ref={account} className='p-3 w-11/12 mr-4 bg-primaryBlack my-3 bg-gradient-to-r from-secondaryGreen to-[#1edfdf1f] rounded-lg focus:outline-none focus:border-none'>
+          {availableAccounts.map(account => <option value={account}>{account}</option>)}
+        </select>
+        <select ref={category} className='p-3 w-11/12 ml-4 bg-primaryBlack my-3 bg-gradient-to-r from-secondaryGreen to-[#1edfdf1f] rounded-lg focus:outline-none focus:border-none'>
+          <option value="Food & Drinks">Food & Drinks</option>
+          <option value="Shopping">Shopping</option>
+          <option value="Housing">Housing</option>
+          <option value="Transportation">Transportation</option>
+          <option value="Vehicle">Vehicle</option>
+          <option value="Life & Entertainment">Life & Entertainment</option>
+          <option value="Communication, PC">Communication, PC</option>
+          <option value="Financial Expenses">Financial Expenses</option>
+          <option value="Investments">Investments</option>
+          <option value="Income">Income</option>
+          <option value="Others">Others</option>
+          <option value="Unknown">Unknown</option>
+        </select>
+      </div>
       <div className='flex w-11/12 my-3 transition-all'>
         <div className={`p-0.5 ${isIncome ? 'gradient-border-1' : ''} mr-6 transition-all hover:-translate-y-1`}>
           <button onClick={() => setIsIncome(true)} className={`p-3 px-12 bg-secondaryBlack ${isIncome ? '' : 'rounded-lg opacity-50'}`}>Income</button>
